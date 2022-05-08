ba18a12c0eec0ae299fa50b7977349e1fae66eeb
ProgrammerIn-wonderland <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2022-05-08 14:39:01 -0400

    updates to url

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='alice@alicesworld.tech',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='',
+    url='https://github.com/ProgrammerIn-wonderland/hmfullpy',
     keywords= ["anime", "hentai", "nsfw", "sfw", "images", "gifs", "wallpaper", "discord", "ahegao", "ass", "neko", "kitsune", "yuri", "panties", "thighs", "foot", "overwatch", "dva", "erotic", "lewdkemo", "lewdneko", "lewdkitsune", "holo", "bj", "spank", "ero", "kawaii", "cute", "waifu", "hmtai", "zettaiRyouiki", "18+", "REST", "API", "Mikun"],
     install_requires=[
           'requests',
