514d068e281e186295f62a7286293f0eb70f1cdd
ProgrammerIn-wonderland <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2022-05-08 14:37:35 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hmfull',
+    version='1.0',
+    license='ISC',
+    author="Alice Addison",
+    author_email='alice@alicesworld.tech',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords= ["anime", "hentai", "nsfw", "sfw", "images", "gifs", "wallpaper", "discord", "ahegao", "ass", "neko", "kitsune", "yuri", "panties", "thighs", "foot", "overwatch", "dva", "erotic", "lewdkemo", "lewdneko", "lewdkitsune", "holo", "bj", "spank", "ero", "kawaii", "cute", "waifu", "hmtai", "zettaiRyouiki", "18+", "REST", "API", "Mikun"],
+    install_requires=[
+          'requests',
+      ],
+
+)
